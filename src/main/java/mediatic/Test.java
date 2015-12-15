feaf18ce37ed06992813e21c767dfe9bc8349b0b
AdrienQuerol <queroladrien@gmail.com>
2015-12-15 11:36:08 +0100

    modif test

--- src/main/java/mediatic/Test.java
+++ src/main/java/mediatic/Test.java
@@ -5,6 +5,6 @@ import com.google.common.base.Strings;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println(Strings.isNullOrEmpty(null));
+		System.out.println(Strings.isNullOrEmpty("eeeee"));
 	}
 }
